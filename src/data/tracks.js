febe206ecea910b0debd2cb681f22832ad3662ff
ayaannasim9 <ayaannasim9@gmail.com>
2025-04-25 02:57:57 +0100

    Added hardcoded tracks

---
+++ src/data/tracks.js
@@ -0,0 +1,41 @@
+// src/data/tracks.js
+export const moodTracks = {
+  chill: [
+    {
+      title: "lofi hip hop radio",
+      url: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
+    },
+    {
+      title: "Chillhop Essentials",
+      url: "https://www.youtube.com/watch?v=5yx6BWlEVcY",
+    },
+  ],
+  sad: [
+    { title: "Sad Hours", url: "https://www.youtube.com/watch?v=5qap5aO4i9A" },
+    {
+      title: "Emotional Piano",
+      url: "https://www.youtube.com/watch?v=3pL5jV9YxDo",
+    },
+  ],
+  happy: [
+    {
+      title: "Feel Good Playlist",
+      url: "https://www.youtube.com/watch?v=ZbZSe6N_BXs",
+    },
+    {
+      title: "Upbeat Indie",
+      url: "https://www.youtube.com/watch?v=d-diB65scQU",
+    },
+  ],
+  party: [
+    { title: "EDM Party", url: "https://www.youtube.com/watch?v=fB8TyLTD7EE" },
+    { title: "Top 40 Mix", url: "https://www.youtube.com/watch?v=kXYiU_JCYtU" },
+  ],
+  focus: [
+    {
+      title: "Focus Playlist",
+      url: "https://www.youtube.com/watch?v=hHW1oY26kxQ",
+    },
+    { title: "Brain Food", url: "https://www.youtube.com/watch?v=WPni755-Krg" },
+  ],
+};
