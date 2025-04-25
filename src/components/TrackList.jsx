28bc235e11196a27bec7691d0b005c099698aa60
ayaannasim9 <ayaannasim9@gmail.com>
2025-04-25 02:58:26 +0100

    Displaying hardcoded tracks for a mood now

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,9 +1,24 @@
 import { moods } from "../data/moods";
+import { moodTracks } from "../data/tracks";
 function TrackList({ selectedMood }) {
   if (!selectedMood) return null;
   if (!moods.includes(selectedMood)) {
     return <p>Unkown mood 😅</p>;
   }
-  return <p>{selectedMood} songs</p>;
+  const tracks = moodTracks[selectedMood];
+  return (
+    <div>
+      <h2>Tracks for the selected mood : </h2>
+      <ul>
+        {tracks.map((track, index) => (
+          <li key={index}>
+            <a href={track.url} target="_blank">
+              {track.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default TrackList;
